e1644b6763c7375f92fab30e3e044bd2338b466d
Lethworgen <Lethworgen@hotmail.com>
2019-11-19 03:54:14 -0800

    Dump of current prototypedb.sql

--- application/prototypedb.sql
+++ application/prototypedb.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.18, for macos10.14 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
 --
 -- Host: localhost    Database: prototypedb
 -- ------------------------------------------------------
@@ -7,7 +7,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -31,16 +31,6 @@ CREATE TABLE `categories` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `categories`
---
-
-LOCK TABLES `categories` WRITE;
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'All'),(2,'Electronics'),(3,'Books'),(4,'Miscellaneous'),(5,'Furniture'),(6,'Services');
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `posts`
 --
 
@@ -61,14 +51,20 @@ CREATE TABLE `posts` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `posts`
+-- Table structure for table `users`
 --
 
-LOCK TABLES `posts` WRITE;
-/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (3,'Chair','A comfy chair to sit in',5,'chair.png'),(4,'Bed','A nice bed to sleep in',5,'bed.jpeg'),(5,'Calc Textbook','Fundamentals of calculus',3,'calcbook.jpeg'),(6,'TV','Big 50\' TV, really big.',2,'tv_image.jpg'),(7,'Wig','A colorful clown wig',4,'wig.jpeg'),(8,'Cracking the Coding interview','a book about interviewing, very popular',3,'ctci.jpg'),(9,'Math: A subtle history','a long book about mathematics history',3,'mathbook_image.jpg'),(10,'Xbox 1','the original xbox, not the new xbox one',2,'xbox.jpeg'),(11,'History tutoring','Paid tutoring for entry level history',6,'tutoring.png'),(12,'Basic Math tutoring','Paid tutoring for entry level math',6,'tutoring.png'),(13,'Physics tutoring','Paid tutoring for entry level Physics',6,'tutoring.png'),(14,'RPG Game tutoring','How to be a programming wizard',6,'tutoring.png');
-/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -79,4 +75,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-08  0:13:10
+-- Dump completed on 2019-11-19  3:52:32
