1a8cc0daa932d4b115ee056a535b8a19da1118e5
Lethworgen <Lethworgen@hotmail.com>
2019-11-19 03:50:09 -0800

    Reverted password to 'password' from local

--- application/app.py
+++ application/app.py
@@ -30,7 +30,7 @@ app.secret_key = 'your secret key'
 
 # Connect to the MySQL database
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '1234'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
 app.config['MYSQL_DATABASE_DB'] = 'prototypedb'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
